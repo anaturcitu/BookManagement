b94abc950015fe99ba8cf1cb874e52c4cd0b5cc8
anaturcitu <ana-maria.turcitu@s.unibuc.ro>
2025-05-06 12:11:30 +0300

    update Spring Security

--- bookmanagement/bookmanagement/src/main/java/com/unibuc/bookmanagement/config/SecurityConfig.java
+++ bookmanagement/bookmanagement/src/main/java/com/unibuc/bookmanagement/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users/register", "/reviews/**", "/books", "/users/login", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/users/register", "/books", "/users/login", "/css/**", "/js/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
