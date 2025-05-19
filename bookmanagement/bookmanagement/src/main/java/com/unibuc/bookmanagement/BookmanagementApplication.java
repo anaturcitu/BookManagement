9e61396f0ab2c0227bbdaedbb391b6acaa510223
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2025-05-19 20:01:55 +0300

    add loggs in BookmanagementApplication

--- bookmanagement/bookmanagement/src/main/java/com/unibuc/bookmanagement/BookmanagementApplication.java
+++ bookmanagement/bookmanagement/src/main/java/com/unibuc/bookmanagement/BookmanagementApplication.java
@@ -2,12 +2,16 @@ package com.unibuc.bookmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class BookmanagementApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(BookmanagementApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookmanagementApplication.class, args);
+		logger.info("Aplicația BookManagement a fost pornită cu succes.");
 	}
-
 }
